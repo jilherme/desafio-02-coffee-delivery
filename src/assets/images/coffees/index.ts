002d11f1d5b202f9cddd488b4aa2728fa8cd7d98
jilherme <rosa.guilhermediogo@gmail.com>
2023-04-01 22:57:43 -0300

    add(assets): coffee images

---
+++ src/assets/images/coffees/index.ts
@@ -0,0 +1,31 @@
+import Americano from './Americano.svg'
+import Arabe from './Arabe.svg'
+import CafeComLeite from './Café com Leite.svg'
+import CafeGelado from './Café Gelado.svg'
+import Cappuccino from './Capuccino.svg'
+import ChocolateQuente from './Chocolate Quente.svg'
+import Cubano from './Cubano.svg'
+import ExpressoCremoso from './Expresso Cremoso.svg'
+import Havaiano from './Havaiano.svg'
+import Irlandes from './Irlandes.svg'
+import Latte from './Latte.svg'
+import Macchiato from './Macchiato.svg'
+import Mochaccino from './Mochaccino.svg'
+import Tradicional from './Tradicional.svg'
+
+export {
+  Americano,
+  Arabe,
+  CafeComLeite,
+  CafeGelado,
+  Cappuccino,
+  ChocolateQuente,
+  Cubano,
+  ExpressoCremoso,
+  Havaiano,
+  Irlandes,
+  Latte,
+  Macchiato,
+  Mochaccino,
+  Tradicional,
+}
